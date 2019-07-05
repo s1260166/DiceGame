d905adc5a4fc3f8d93af4bdd1b8b601f0e2feb92
s1260166 <s1260166@std4dc21>
2019-07-05 12:21:42 +0900

    Greetings added

--- diceProgram.java
+++ diceProgram.java
@@ -1,10 +1,31 @@
 import java.util.Random;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class diceProgram{
 
     public static void main(String[] args){
 	int count1,count2;
 	Random rand = new Random();
+
+
+	InputStreamReader isr = new InputStreamReader(System.in);
+	BufferedReader br = new BufferedReader(isr);
+	
+	System.out.println("What is your name?");
+	System.out.print("> ");
+
+	String str = null;
+	try{
+	    str = br.readLine();
+	    br.close();
+	}catch(IOException e){
+	    e.printStackTrace();
+	}
+	System.out.println("Hello, "+str+"!");
+	
 	
 	System.out.println("Rolling the dice...");
 	
@@ -19,6 +40,9 @@ public class diceProgram{
 	System.out.println("Die 2: " + count2);
 
 	System.out.println("Total value: "+ (count1 + count2) );
+
+
+	
     }
 
 }
